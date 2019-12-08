c9ba84fd8510925d2a0d0cc8adeedcbf0f2145f6
Kirtan Patel <kirtanpatl@gmail.com>
2019-12-08 03:04:57 -0500

    Test test data

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 """
 Read in the data and evaluate the models
 """
+import time
 
 import numpy as np
 import util
 from nb import NaiveBayes
 from perceptron import Perceptron
 from knn import KNN
-from features import rawPixelFeature, rowPixelFeature, gridFeature, multipleGridFeatures
+from features import rawPixelFeature, rowPixelFeature, gridFeature, multipleGridFeatures, imageCompressor
 
 
 # TODO: first, implement these functions
@@ -69,38 +70,145 @@ def split(array):
 	print(s)
 	return s
 
-if __name__ == '__main__':
-	n_samples = 450
-	n_validation = n_samples // 10
-	faces_list = read_faces_file("facedata/facedatatrain", n_samples)
+def digitClassification(percent: int):
+	n_total_digits = 450
+	n_samples_digits = int(n_total_digits * (percent/100))
+	n_testing_digits = 1000
+	digit_list = read_digits_file("digitdata/trainingimages", n_samples_digits)
+	digit_test_list = read_digits_file("digitdata/testimages", n_testing_digits)
 
-	y = np.array(util.loadLabelsFile("facedata/facedatatrainlabels", n_samples))
-	train_y = y[:-n_validation]
-	validation_y = y[-n_validation:]
+	y = np.array(util.loadLabelsFile("digitdata/traininglabels", n_samples_digits))
 
-	x = perceptron_faces_features(faces_list)
-	train_x = x[:-n_validation]
-	validation_x = x[-n_validation:]
+
+	train_y = y
+	test_y = np.array(util.loadLabelsFile("digitdata/testlabels", n_testing_digits))
+
+	x = perceptron_faces_features(digit_list)
+	train_x = x
+	test_x = perceptron_faces_features(digit_test_list)
 	p_model = Perceptron(100)
 	p_model.train(train_x, train_y)
-	matches = list(p_model.predict(validation_x) == validation_y).count(True)
-	accuracy = matches / n_validation
+	matches = list(p_model.predict(test_x) == test_y).count(True)
+	accuracy = matches / n_testing_digits
 	print('Perceptron accuracy:', 100 * accuracy, '%')
 
-	x = nb_faces_features(faces_list)
-	train_x = x[:-n_validation]
-	validation_x = x[-n_validation:]
+	x = nb_faces_features(digit_list)
+	train_x = x
+	test_x = nb_faces_features(digit_test_list)
 	nb_model = NaiveBayes()
 	nb_model.train(train_x, train_y)
-	matches = list(nb_model.predict(validation_x) == validation_y).count(True)
-	accuracy = matches / n_validation
+	matches = list(nb_model.predict(test_x) == test_y).count(True)
+	accuracy = matches / n_testing_digits
 	print('nb accuracy:', 100 * accuracy, '%')
 
-	x = knn_faces_features(faces_list)
-	train_x = x[:-n_validation]
-	validation_x = x[-n_validation:]
+	x = knn_faces_features(digit_list)
+	train_x = x
+	test_x = knn_faces_features(digit_test_list)
 	knn_model = KNN()
 	knn_model.train(train_x, train_y)
-	matches = list(knn_model.predict(validation_x) == validation_y).count(True)
-	accuracy = matches / n_validation
+	matches = list(knn_model.predict(test_x) == test_y).count(True)
+	accuracy = matches / n_testing_digits
 	print('KNN accuracy:', 100 * accuracy, '%')
+
+def faceClassification(percent: int):
+	n_total_faces = 450
+	n_samples_faces = int(n_total_faces * (percent / 100))
+	n_testing_digits = 150
+	face_list = read_faces_file("facedata/facedatatrain", n_samples_faces)
+	face_test_list = read_faces_file("facedata/facedatatest", n_testing_digits)
+
+	y = np.array(util.loadLabelsFile("facedata/facedatatrainlabels", n_samples_faces))
+
+	train_y = y
+	test_y = np.array(util.loadLabelsFile("facedata/facedatatestlabels", n_testing_digits))
+
+	x = perceptron_faces_features(face_list)
+	train_x = x
+	test_x = perceptron_faces_features(face_test_list)
+	p_model = Perceptron(100)
+	p_model.train(train_x, train_y)
+	matches = list(p_model.predict(test_x) == test_y).count(True)
+	accuracy = matches / n_testing_digits
+	print('Perceptron accuracy:', 100 * accuracy, '%')
+
+	x = nb_faces_features(face_list)
+	train_x = x
+	test_x = nb_faces_features(face_test_list)
+	nb_model = NaiveBayes()
+	nb_model.train(train_x, train_y)
+	matches = list(nb_model.predict(test_x) == test_y).count(True)
+	accuracy = matches / n_testing_digits
+	print('nb accuracy:', 100 * accuracy, '%')
+
+	x = knn_faces_features(face_list)
+	train_x = x
+	test_x = knn_faces_features(face_test_list)
+	knn_model = KNN()
+	knn_model.train(train_x, train_y)
+	matches = list(knn_model.predict(test_x) == test_y).count(True)
+	accuracy = matches / n_testing_digits
+	print('KNN accuracy:', 100 * accuracy, '%')
+
+if __name__ == '__main__':#demonstration
+
+	print("Digit classification")
+	print()
+	total_start_time = time.time()
+	for x in range(10, 101, 10):
+		print(str(x) + '%' + ' of training data.')
+		start_time = time.time()
+
+		digitClassification(x)
+		elapsed_time = time.time() - start_time
+		print('%.3f'%(elapsed_time) + " seconds")
+		print()
+
+	print("Face or not face classification")
+	print()
+	for x in range(10, 101, 10):
+		print(str(x) + '%' + ' of training data.')
+		start_time = time.time()
+
+		faceClassification(x)
+		elapsed_time = time.time() - start_time
+		print('%.3f'%(elapsed_time) + " seconds")
+		print()
+
+	elapsed_time = time.time() - total_start_time
+	print("TOTAL TIME "+'%.3f'%(elapsed_time) + " seconds")
+
+
+	# n_samples = 450
+	# n_validation = n_samples // 10
+	# faces_list = read_faces_file("facedata/facedatatrain", n_samples)
+	#
+	# y = np.array(util.loadLabelsFile("facedata/facedatatrainlabels", n_samples))
+	# train_y = y[:-n_validation]
+	# validation_y = y[-n_validation:]
+	#
+	# x = perceptron_faces_features(faces_list)
+	# train_x = x[:-n_validation]
+	# validation_x = x[-n_validation:]
+	# p_model = Perceptron(100)
+	# p_model.train(train_x, train_y)
+	# matches = list(p_model.predict(validation_x) == validation_y).count(True)
+	# accuracy = matches / n_validation
+	# print('Perceptron accuracy:', 100 * accuracy, '%')
+	#
+	# x = nb_faces_features(faces_list)
+	# train_x = x[:-n_validation]
+	# validation_x = x[-n_validation:]
+	# nb_model = NaiveBayes()
+	# nb_model.train(train_x, train_y)
+	# matches = list(nb_model.predict(validation_x) == validation_y).count(True)
+	# accuracy = matches / n_validation
+	# print('nb accuracy:', 100 * accuracy, '%')
+	#
+	# x = knn_faces_features(faces_list)
+	# train_x = x[:-n_validation]
+	# validation_x = x[-n_validation:]
+	# knn_model = KNN()
+	# knn_model.train(train_x, train_y)
+	# matches = list(knn_model.predict(validation_x) == validation_y).count(True)
+	# accuracy = matches / n_validation
+	# print('KNN accuracy:', 100 * accuracy, '%')
